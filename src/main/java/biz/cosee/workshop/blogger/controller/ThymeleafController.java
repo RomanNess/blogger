96d5963182148cd98a27fa663ed8b19260d15a4d
Roman Ness <roman.ness@cosee.biz>
2020-04-06 15:55:48 +0200

    add first thymeleaf template

---
+++ src/main/java/biz/cosee/workshop/blogger/controller/ThymeleafController.java
@@ -0,0 +1,23 @@
+package biz.cosee.workshop.blogger.controller;
+
+import biz.cosee.workshop.blogger.dto.ArticleDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+
+@Controller
+public class ThymeleafController {
+
+    @GetMapping("/")
+    public String main(Model model) {
+        model.addAttribute("user", "John Doe");
+        model.addAttribute("articles", Arrays.asList(
+                ArticleDto.builder().id(0L).title("title1").content("content1").build(),
+                ArticleDto.builder().id(0L).title("title2").content("content2").build()
+        ));
+
+        return "index";
+    }
+}
