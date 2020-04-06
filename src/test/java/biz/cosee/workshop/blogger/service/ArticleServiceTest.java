6a3a00730e301c6dd46d8bd7530295265d988524
Roman Ness <roman.ness@cosee.biz>
2020-04-06 09:17:31 +0200

    setup h2 db driven unit tests

---
+++ src/test/java/biz/cosee/workshop/blogger/service/ArticleServiceTest.java
@@ -0,0 +1,27 @@
+package biz.cosee.workshop.blogger.service;
+
+import biz.cosee.workshop.blogger.AbstractSpringContextTest;
+import biz.cosee.workshop.blogger.dto.ArticleDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ArticleServiceTest extends AbstractSpringContextTest {
+
+    @Autowired
+    private ArticleService articleService;
+
+    @Test
+    void insert() {
+        ArticleDto dto = new ArticleDto();
+        dto.setTitle("The title");
+        dto.setContent("The content.");
+
+        ArticleDto articleDto = articleService.create(dto);
+        assertThat(articleDto).isNotNull();
+        assertThat(articleDto.getId()).isNotNull();
+        assertThat(articleDto.getTitle()).isEqualTo("The title");
+        assertThat(articleDto.getContent()).isEqualTo("The content.");
+    }
+}
